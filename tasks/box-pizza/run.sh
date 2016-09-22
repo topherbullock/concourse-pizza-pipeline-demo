b664f23b2e09a0015906fb69e6a68db6a564286f
Topher Bullock <topherbullock@gmail.com>
2016-09-22 15:29:48 -0400

    add delivery job

---
+++ tasks/box-pizza/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mv ./pizza/pizza.png ./boxed-pizza
+
+pushd boxed-pizza
+  touch Staticfile
+
+  echo "applications:
+  - name: gimme-pizza
+    memory: 512M" > manifest.yml
+
+  echo "<!DOCTYPE html>
+<html>
+  <body>
+    <h1> PIZZA!!! <h1>
+    <img src=\"./pizza.png\" />
+  </body>
+</html>
+"
+
+popd
